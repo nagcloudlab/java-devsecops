08173a03d18f5f6ea5829986d35bf674b7f7aab3
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-21 13:40:54 +0530

    day3 commit

--- object-patterns/src/main/java/com/example/FactoryExample.java
+++ object-patterns/src/main/java/com/example/FactoryExample.java
@@ -1,2 +1,63 @@
-package com.example;public class FactoryExample {
+package com.example;
+
+interface Food {
+}
+
+class VegFood implements Food {
+}
+
+class NonVegFood implements Food {
+}
+
+
+class FoodFactory {
+    public static Food createFood(String foodType) {
+        if (foodType.equals("veg")) {
+            return new VegFood();
+        } else if (foodType.equals("non-veg")) {
+            return new NonVegFood();
+        } else {
+            throw new IllegalArgumentException("Invalid food type");
+        }
+    }
+}
+
+
+public class FactoryExample {
+    public static void main(String[] args) {
+
+        //---------------------------------------
+        // module-1
+        //---------------------------------------
+
+        String foodType = "veg"; // | "non-veg"
+        Food food = FoodFactory.createFood(foodType);
+
+        if (food instanceof VegFood) {
+            System.out.println("Veg food created");
+        } else if (food instanceof NonVegFood) {
+            System.out.println("Non-veg food created");
+        } else {
+            System.out.println("Unknown food type");
+        }
+
+
+        //---------------------------------------
+        // module-2
+        //----------------------------------------
+
+        foodType = "non-veg"; // | "non-veg"
+        food = FoodFactory.createFood(foodType);
+
+
+        if (food instanceof VegFood) {
+            System.out.println("Veg food created");
+        } else if (food instanceof NonVegFood) {
+            System.out.println("Non-veg food created");
+        } else {
+            System.out.println("Unknown food type");
+        }
+
+
+    }
 }
--- object-patterns/src/main/java/com/example/Q.java
+++ object-patterns/src/main/java/com/example/Q.java
@@ -1,2 +1,102 @@
-package com.example;public class Q {
+package com.example;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+interface DoorListener {
+    void on();
+
+    void off();
+}
+
+// service
+class Light implements DoorListener {
+    public void on() {
+        System.out.println("Light is on");
+    }
+
+    public void off() {
+        System.out.println("Light is off");
+    }
+}
+
+//service
+class AC implements DoorListener {
+    public void on() {
+        System.out.println("AC is on");
+    }
+
+    public void off() {
+        System.out.println("AC is off");
+    }
+}
+
+//service
+class Projector implements DoorListener {
+    public void on() {
+        System.out.println("Projector is on");
+    }
+
+    public void off() {
+        System.out.println("Projector is off");
+    }
+}
+
+
+class Door {
+
+    //private DoorListener doorListener;
+    private ArrayList<DoorListener> doorListeners = new ArrayList<>();
+
+    public void addDoorListener(DoorListener doorListener) {
+        doorListeners.add(doorListener);
+    }
+
+    public void removeDoorListener(DoorListener doorListener) {
+        doorListeners.remove(doorListener);
+    }
+
+    public void open() {
+        System.out.println("Door is opened");
+//        for (DoorListener doorListener : doorListeners) {
+//            doorListener.on();
+//        }
+    }
+
+    public void close() {
+        System.out.println("Door is closed");
+        for (DoorListener doorListener : doorListeners) {
+            doorListener.off();
+        }
+    }
+}
+
+public class Q {
+    public static void main(String[] args) throws InterruptedException {
+
+        Door door = new Door();
+
+        Light light = new Light();
+        AC ac = new AC();
+        Projector projector = new Projector();
+
+        door.addDoorListener(light);
+        door.addDoorListener(ac);
+        door.addDoorListener(projector);
+
+        TimeUnit.SECONDS.sleep(3);
+        door.open();
+        TimeUnit.SECONDS.sleep(3);
+        door.close();
+
+
+        // Remove the light listener
+        door.removeDoorListener(projector);
+
+        TimeUnit.SECONDS.sleep(3);
+        door.open();
+        TimeUnit.SECONDS.sleep(3);
+        door.close();
+
+    }
 }
