3c08c1f99cc59544e26584d0e8d735b80874d013
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-22 13:54:38 +0530

    factory pattern

---
+++ object-patterns/src/main/java/com/example/Problem.java
@@ -0,0 +1,66 @@
+package com.example;
+
+// Abstract-Class or Interface
+
+interface Connection {
+}
+
+class PostgresConnection implements Connection {
+}
+
+class CassandraConnection implements Connection {
+}
+
+// Factory Class
+class ConnectionFactory {
+
+    //Factory-Method
+    public static Connection getConnection(String dbType) {
+        if (dbType.equals("postgres")) {
+            //..
+            // driver
+            // url
+            // username
+            // password
+            //....
+            PostgresConnection connection = new PostgresConnection();
+            return connection;
+        } else if (dbType.equals("cassandra")) {
+            //..
+            // driver
+            // url
+            // username
+            // password
+            //....
+            CassandraConnection connection = new CassandraConnection();
+            return connection;
+        } else {
+            throw new IllegalArgumentException("invalid dabatse type");
+        }
+    }
+}
+
+
+public class Problem {
+
+    public static void main(String[] args) {
+
+
+        //-------------------------------------------------------
+        // team-1 : module ( UPI )
+        //-------------------------------------------------------
+
+        String dbType = "postgres"; // | cassandraa
+        Connection connection = ConnectionFactory.getConnection(dbType);
+
+        //-------------------------------------------------------
+        // team-2 : module ( NEFT )
+        //-------------------------------------------------------
+
+        dbType = "cassandra"; // | postgres
+        connection = ConnectionFactory.getConnection(dbType);
+
+
+    }
+
+}
