107ec0301280a43f19b70ae4741444a7fe8ff715
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-27 13:39:54 +0530

    data processing exercise

--- stream-library/src/main/java/com/example/Exercise1.java
+++ stream-library/src/main/java/com/example/Exercise1.java
@@ -4,6 +4,20 @@ import com.example.model.Dish;
 
 import java.util.List;
 
+
+// data processing code
+// - filtering
+// - mapping aka transforming
+// - aggregation
+// - grouping
+// - sorting
+// - partitioning
+// - min, max, sum, average
+// - allMatch, anyMatch, noneMatch
+// - findFirst, findAny
+// - collecting to a list, set, map, etc.
+
+
 public class Exercise1 {
 
     public static void main(String[] args) {
@@ -11,20 +25,8 @@ public class Exercise1 {
 
         List<Dish> menu=Dish.menu; // immutable list of dishes
 
-        // data processing code
-        // - filtering
-        // - mapping aka transforming
-        // - aggregation
-        // - grouping
-        // - sorting
-        // - partitioning
-        // - min, max, sum, average
-        // - allMatch, anyMatch, noneMatch
-        // - findFirst, findAny
-        // - collecting to a list, set, map, etc.
-
-
-        // - get low cal ( < 400 ) dish'r names sorted by cal into new List
+        // - get low cal ( < 400 ) dish's names sorted by cal into new List
+        // output : season fruit, rice
 
 
 
