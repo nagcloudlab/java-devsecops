8324c5d926b8ae966d5a18518b8c71fc4c35e174
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-05-29 13:15:43 +0530

    transfer-service

---
+++ transfer-service-v2/src/main/java/com/example/reposotory/JpaAccountRepository.java
@@ -0,0 +1,29 @@
+package com.example.reposotory;
+
+import com.example.model.Account;
+import org.apache.log4j.Logger;
+
+/**
+ * author: dev-1
+ */
+
+public class JdbcAccountRepository {
+
+    private static Logger logger = Logger.getLogger("transfer-service"); // singleton logger
+
+    public JdbcAccountRepository() {
+        logger.info("JdbcAccountRepository initialized");
+    }
+
+    public Account findByAccountByNumber(String number) {
+        logger.info("Finding account by number: " + number);
+        // Simulate loading an account from a database
+        return new Account(number, 1000.0);
+    }
+
+    public void saveAccount(Account account) {
+        logger.info("Saving account: " + account.getNumber());
+        // Simulate saving an account to a database
+    }
+
+}
