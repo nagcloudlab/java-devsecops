81ba0707401af9007461eaf434dfcd731c18f633
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-06-05 10:25:45 +0530

    maven multi-module project

---
+++ play-with-devsecops/calculator-util/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+
+}
